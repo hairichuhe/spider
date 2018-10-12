467fa1b08912b7ac507e1095c843e18f72625fb1
hairichuhe <14320794@qq.com>
2018-10-12 18:31:46 +0800

    1012

--- main.go
+++ main.go
@@ -10,17 +10,14 @@ func main() {
 	// Instantiate default collector
 	c := spider.NewCollector(
 		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		spider.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+		spider.AllowedDomains("www.gszfcg.gansu.gov.cn"),
 	)
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *spider.HTMLElement) {
 		link := e.Attr("href")
 		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
+		fmt.Println("Link found: %q -> %s\n", e.Text, link)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -28,6 +25,12 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	c.OnError(func(_ *spider.Response, err error) {
+
+		fmt.Println("Something went wrong:", err)
+
+	})
+
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("http://www.gszfcg.gansu.gov.cn/web/doSearch.action?op=%271%27&articleSearchInfoVo.title=%25E7%25BD%2591%25E7%25BB%259C&articleSearchInfoVo.bidcode=&articleSearchInfoVo.proj_name=&articleSearchInfoVo.agentname=&articleSearchInfoVo.buyername=&articleSearchInfoVo.division=&articleSearchInfoVo.classname=12804&articleSearchInfoVo.dtype=6209&articleSearchInfoVo.releasestarttime=&articleSearchInfoVo.releaseendtime=&articleSearchInfoVo.tflag=1")
 }
